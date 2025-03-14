d70dcdedaf4e3e04f972ad38a1a1f97ff129ce7c
Viral Vaghela <viralvaghela8577@gmail.com>
2025-03-14 22:37:11 +0530

    update Cloudinary configuration: rename cloud name environment variable

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Your Cloudinary cloud name
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME_NEW, // Your Cloudinary cloud name
   api_key: process.env.CLOUDINARY_API_KEY,      // Your API key
   api_secret: process.env.CLOUDINARY_API_SECRET // Your API secret
 });
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // Configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME_NEW,
     api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
     api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET
 });
